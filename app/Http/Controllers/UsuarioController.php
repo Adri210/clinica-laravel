e6f08647b9811b7c77231465ae7adec22e50cb31
Adriani Delfino <141968057+Adri210@users.noreply.github.com>
2025-05-07 19:42:30 -0300

    Primeiro commit - Projeto Laravel

---
+++ app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UsuarioController extends Controller
+{
+
+    public function index()
+    {
+    return view('usuarios.index');
+    }
+
+    public function create()
+{
+    return view('usuarios.create');
+}
+
+
+}
