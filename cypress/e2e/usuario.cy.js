4122cac082158f696a798f857934c1364219f489
Dyeison-Felipe <dyeisonfc@gmail.com>
2025-06-10 18:49:38 -0300

    removido tetse

--- cypress/e2e/usuario.cy.js
+++ cypress/e2e/usuario.cy.js
@@ -77,36 +77,4 @@ describe('Usuário', () => {
     cy.contains('Usuário excluído com sucesso!').should('be.visible');
   });
 
-  it('deve exibir modal de erro ao falhar no cadastro (simulação)', () => {
-    // Intercepta a requisição POST e força um erro 500
-    cy.intercept('POST', '/usuarios', {
-        statusCode: 500,
-        body: {
-            success: false,
-            message: 'Ocorreu um erro ao salvar o usuário. Tente novamente.'
-        }
-    }).as('postUsuario');
-
-    cy.visit('/usuarios/create');
-    cy.get('input[name=nome]').type('Teste');
-    cy.get('input[name=sobrenome]').type('Erro');
-    cy.get('input[name=data_nascimento]').type('1990-01-01');
-    cy.get('input[name=cep]').type('12345-678');
-    cy.get('input[name=rua]').type('Rua Teste');
-    cy.get('input[name=numero]').type('123');
-    cy.get('input[name=bairro]').type('Centro');
-    cy.get('input[name=cidade]').type('São Paulo');
-    cy.get('input[name=estado]').type('SP');
-    cy.get('select[name=tipo_usuario]').select('recepcionista');
-    cy.get('input[name=senha]').type('senha123');
-    cy.get('input[name=senha_confirmation]').type('senha123');
-    cy.get('button[type=submit]').contains('Cadastrar').click();
-    
-    // Espera a requisição ser interceptada
-    cy.wait('@postUsuario');
-    
-    // Verifica se o modal de erro está visível
-    cy.get('#errorToast').should('be.visible');
-    cy.get('#errorMessage').should('contain', 'Ocorreu um erro ao salvar o usuário');
-});
 });
